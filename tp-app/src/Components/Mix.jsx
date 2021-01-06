7d1deb30ac7bf0ec6424c46b4218fd8afc4fbc55
MaximeMalecot <92maximemalecot@gmail.com>
2021-01-06 16:22:41 +0100

    Changes on gitignore

--- tp-app/src/Components/Mix.jsx
+++ tp-app/src/Components/Mix.jsx
@@ -1,9 +1,21 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
 
 export default function Mix(){
+    const [cocktail, setCocktail] = useState();
+    const [music, setMusic] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+            .then((res) => res.json())
+            .then((res) => { 
+                setCocktail(res)
+            })
+            .catch((e) => console.error(e));
+        fetchData();
+    }, [])
     return(
         <div>
-            mix
+            {JSON.stringify(cocktail)}
         </div>
     )
 }
